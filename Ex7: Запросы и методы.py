ecc6a8515b51c6cd864adf01f68aca62ac678c13
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-13 23:16:53 +0300

    Update Ex7: Запросы и методы.py

--- Ex7: Запросы и методы.py
+++ Ex7: Запросы и методы.py
@@ -14,7 +14,21 @@ print(response2.text)
 response3 = requests.get(url=url, params=payload1)
 print(response3.text)
 
-
-
-
-
+method = [{"method": "GET"}, {"method": "POST"}, {"method": "PUT"}, {"method": "DELETE"}]
+for i in method:
+    response4 = requests.get(url=url, params=i)
+    print(response4.text)
+    if response4 == {"success": "!"}:
+        print()
+    response5 = requests.post(url=url, data=i)
+    print(response5.text)
+    if response5 == {"success": "!"}:
+        print()
+    response6 = requests.put(url=url, data=i)
+    print(response6.text)
+    if response6 == {"success": "!"}:
+        print()
+    response7 = requests.delete(url=url, data=i)
+    print(response7.text)
+    if response7 == {"success": "!"}:
+        print()
