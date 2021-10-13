83fbf73c4b8f9720161de7218a699165eca75875
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-13 18:20:41 +0300

    Create Ex7: Запросы и методы.py

---
+++ Ex7: Запросы и методы.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+
+payload1 = {"method": "get"}
+payload2 = {"method": "get"}, {"method": "post"}, {"method": "put"}, {"method": "delete"}
+
+response1 = requests.Request(url=url, params=payload1)
+print(response1)
+response2 = requests.head(url=url, data={"method": "head"})
+print(response2)
+response3 = requests.get(url=url, params=payload1)
+print(response3)
