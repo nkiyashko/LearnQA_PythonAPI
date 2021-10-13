d77c66eb9b333f324bc98cdea699135895e4476b
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-13 19:07:41 +0300

    Update Ex7: Запросы и методы.py

--- Ex7: Запросы и методы.py
+++ Ex7: Запросы и методы.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
 
 payload1 = {"method": "get"}
-payload2 = {"method": "get"}, {"method": "post"}, {"method": "put"}, {"method": "delete"}
+payload2 = [{"method": "get"}, {"method": "post"}, {"method": "put"}, {"method": "delete"}]
 
 response1 = requests.Request(url=url, params=payload1)
 print(response1)
@@ -11,3 +12,8 @@ response2 = requests.head(url=url, data={"method": "head"})
 print(response2)
 response3 = requests.get(url=url, params=payload1)
 print(response3)
+response4 = requests.get(url=url, params=payload1)
+print(response4.text)
+
+
+
