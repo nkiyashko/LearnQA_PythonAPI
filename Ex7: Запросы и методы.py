3251a26ff11afb001763d9e36c10a1548bb24f09
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-15 14:11:26 +0300

    Update Ex7: Запросы и методы.py

--- Ex7: Запросы и методы.py
+++ Ex7: Запросы и методы.py
@@ -20,10 +20,13 @@ for i in method:
     response4 = requests.get(url=url, params=i)
     #print(response4.text)
     a = response4.json()
-    print(a)
-    b = a['success'][0]
-    if b == "!":
-        print(f'Answer is Succeed in get query with {i} params')
-    elif:
-        a = response4.raise_for_status(JSONDecodeError)
+    #print(a)
+    key = a['success'][0]
+    if key == "!":
+        print(f'Отправили Get запрос с параметром {i}')
+    else:
+        print(f'Тип запроса не совпадает с параметром {i}')
+
+
+
 
