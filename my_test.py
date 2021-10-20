aee4439082dc47da9f79b97cf6f99ba21ca49a0d
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-20 17:45:17 +0300

    ver 2.0

--- my_test.py
+++ my_test.py
@@ -1,5 +1,4 @@
 class TestPhrase:
     def test_length_phrase(self):
-        print("Пожалуйста введите фразу, не длинее 15 символов")
-        phrase = input("Set a phrase: ")
-        assert len(phrase) <= 14
+        phrase = input("Пожалуйста введите фразу, не длинее 15 символов: ")
+        assert len(phrase) <= 14, f"Фраза оказалась 15 символов или длиннее"
