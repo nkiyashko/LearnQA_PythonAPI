e3222044b9cef801b0439a8ae88f4d4eb1d73ff8
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-22 10:43:57 +0300

    ex13

--- test file.py
+++ test file.py
@@ -1,7 +1,13 @@
-import requests
+
+expected_value = [{'platform': 'Mobile', 'browser': 'No', 'device': 'Android'},
+                          {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'},
+                          {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'},
+                          {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'},
+                          {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'}]
+for value in (expected_value):
+    expected_platform = value.get('platform')
+    expected_browser = value.get('browser')
+    expected_device = value.get('device')
+    print(expected_platform, expected_browser, expected_device)
 
 
-response = requests.get("https://playground.learnqa.ru/api/homework_header")
-print(response.headers)
-header = response.json()
-print(header)
