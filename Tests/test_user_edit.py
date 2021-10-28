0d3c315ca09942e5cac1f98679bac9bb196dbd2f
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-28 20:27:44 +0300

    edit user ver 1

--- Tests/test_user_edit.py
+++ Tests/test_user_edit.py
@@ -7,13 +7,13 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # Register new user
         register_data = self.prepare_registration_date()
-        response1 = requests.post("http://playground.learnqa.ru/api/user/", data=register_data)
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_keys(response1, id)
+        Assertions.assert_json_has_key(response1, "id")
 
         email = register_data['email']
-        first_name = register_data['first_name']
+        first_name = register_data['firstName']
         password = register_data['password']
         user_id = self.get_json_value(response1, "id")
 
@@ -22,31 +22,31 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post("http://playground.learnqa.ru/api/login", data=login_data)
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
         # Edit
         new_name = "Changed name"
+
         response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid},
-                                 data={"first_name": new_name}
+                                 data={"firstName": new_name}
                                  )
 
         Assertions.assert_code_status(response3, 200)
 
-
         # GET
-        response4 = requests.get(f"htps://playground.learnqa.ru/api/user/{user_id}",
+        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
 
         Assertions.assert_json_value_by_name(
             response4,
-            'first_name',
+            'firstName',
             new_name,
             "Wrong name of the user after edit"
         )
--- test_file.py
+++ test_file.py
@@ -2,36 +2,13 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
-     # Register new user
+        # Register new user
         register_data = self.prepare_registration_date()
-        response1 = requests.post("http://playground.learnqa.ru/api/user/", data=register_data)
-
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_keys(response1, id)
-
-        email = register_data['email']
-        first_name = register_data['first_name']
-        password = register_data['password']
-        user_id = self.get_json_value(response1, "id")
-
-        # Login
-        login_data = {
-            'email': email,
-            'password': password
-        }
-        response2 = requests.post("http://playground.learnqa.ru/api/login", data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
-
-        # Edit
-        new_name = "Changed name"
-        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
-                             headers={"x-csrf-token": token},
-                             cookies={"auth_sid": auth_sid},
-                             data={"first_name": new_name}
-                             )
-
-        Assertions.assert_code_status(response3, 200)
+        print(register_data)
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        print(response1.text)
+        # Assertions.assert_code_status(response1, 200)
+        # Assertions.assert_json_has_keys(response1, id)
