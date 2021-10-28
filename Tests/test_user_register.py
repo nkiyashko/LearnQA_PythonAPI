291d28a64e97e9b30f28199f46f5bca7baf69d76
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-28 21:17:36 +0300

    везде добавил MyRequests

--- Tests/test_user_register.py
+++ Tests/test_user_register.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from datetime import datetime
@@ -18,16 +18,10 @@ class TestUserRegister(BaseCase):
         ({'password': '123', 'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': ""})
     ]
 
-    # def setup(self):
-    #     base_part = "learnqa"
-    #     domain = "example.com"
-    #     random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-    #     self.email = f"{base_part}{random_part}@{domain}"
-
     def test_create_user_successfully(self):
         data = self.prepare_registration_date()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         # print(response.content)
@@ -44,7 +38,7 @@ class TestUserRegister(BaseCase):
         #     'email': email
         # }
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
@@ -58,7 +52,7 @@ class TestUserRegister(BaseCase):
             'lastName': 'learnqa',
             'email': 'vinkotovexample.com'
         }
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
 
@@ -70,7 +64,7 @@ class TestUserRegister(BaseCase):
             'lastName': 'learnqa',
             'email': self.prepare_registration_date()
         }
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         print(response.content)
         Assertions.assert_code_status(response, 400)
@@ -84,7 +78,7 @@ class TestUserRegister(BaseCase):
             'lastName': 'learnqa',
             'email': self.prepare_registration_date()
         }
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         print(response.content)
         Assertions.assert_code_status(response, 400)
@@ -93,6 +87,6 @@ class TestUserRegister(BaseCase):
     @pytest.mark.parametrize('data', data)
     def test_create_user_without_param(self, data):
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
