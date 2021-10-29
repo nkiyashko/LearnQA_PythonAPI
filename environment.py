4050867cb125305b577b7567bab10eacf1855bfc
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-29 10:46:33 +0300

    Настроил окружение

---
+++ environment.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Environment:
+    DEV = 'dev'
+    PROD = 'prod'
+
+    URLS = {
+        DEV: "https://playgound.learnqa.ru/ajax/api_dev",
+        PROD: "https://playgound.learnqa.ru/ajax/api"
+    }
+
+    def __init__(self):
+        try:
+            self.env = os.environ['ENV']
+        except KeyError:
+            self.env = self.DEV
+
+    def get_base_url(self):
+        if self.env in self.URLS:
+            return self.URLS[self.env]
+        else:
+            raise Exception(f"Unknown value of ENV variable {self.env}")
+
+
+ENV_OBJECT = Environment()
+
