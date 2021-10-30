736d751c7133a5ccb240f8523b9dc5c65d3612a1
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2021-10-30 18:39:28 +0300

    ex17

--- Tests/test_user_edit.py
+++ Tests/test_user_edit.py
@@ -50,3 +50,88 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+# - Попытаемся изменить данные пользователя, будучи неавторизованными
+    def test_edit_non_authorized_user(self):
+        new_name = "Changed name"
+
+        response3 = MyRequests.put(f"/user/2",
+                                   headers={"x-csrf-token": ""},
+                                   cookies={"auth_sid": "auth_sid"},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+
+# - Попытаемся изменить данные пользователя, будучи авторизованными другим пользователем
+    def test_edit_user_via_authorized_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+        new_name = "Changed name"
+
+        response2 = MyRequests.put(f"/user/{user_id_from_auth_method}1",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response2, 400)
+
+# - Попытаемся изменить email пользователя, будучи авторизованными тем же пользователем, на новый email без символа @
+    def test_edit_users_email_without_add_simbol(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+        new_name = "Changed name"
+        new_email = "vinkotovexample.com"
+        response2 = MyRequests.put(f"/user/{user_id_from_auth_method}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name,
+                                         "email": new_email}
+                                   )
+
+        Assertions.assert_code_status(response2, 400)
+
+# - Попытаемся изменить firstName пользователя, будучи авторизованными тем же пользователем, на очень короткое
+# значение в один символ
+    def test_edit_users_firstname_to_short(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+        new_name = "C"
+
+        response2 = MyRequests.put(f"/user/{user_id_from_auth_method}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response2, 400)
+
+
